48195fa7d7cd0e6cc4b1495575cad99ae49b00ff
zeeberry <zeeberry@Augusta.local>
2013-01-14 19:36:33 -0500

    about page added

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/nosotros')
+def about():
+    return render_template('nosotros.html')
+
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
